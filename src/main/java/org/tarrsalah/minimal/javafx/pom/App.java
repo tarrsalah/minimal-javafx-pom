6868277884e7934d34fc7e8fdd112da5877f4268
tarrsalah <tarrsalah@gmail.com>
2013-04-30 13:19:57 +0100

    minimal javafx maven setup

---
+++ src/main/java/org/tarrsalah/minimal/javafx/pom/App.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2013 tarrsalah.org.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.tarrsalah.minimal.javafx.pom;
+
+import java.util.ResourceBundle;
+import javafx.application.Application;
+import javafx.scene.SceneBuilder;
+import javafx.scene.control.ButtonBuilder;
+import javafx.scene.layout.BorderPaneBuilder;
+import javafx.stage.Stage;
+
+/**
+ * App.java (UTF-8)
+ *
+ * Apr 30, 2013
+ *
+ * @author tarrsalah.org
+ */
+public class App extends Application {
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage stage) {
+		
+		String cssFile = getClass().getClassLoader().getResource(ResourceBundle.getBundle("project").getString("cssFile")).toExternalForm();
+
+		stage.setScene(SceneBuilder.create()
+				.root(BorderPaneBuilder.create()
+						.center(ButtonBuilder.create()
+								.text("red button")
+								.id("red_btn")	
+								.build())
+						.build())
+				.width(400)
+				.height(380)
+				.stylesheets(cssFile)
+				.build());		
+		stage.show();
+	}
+}
